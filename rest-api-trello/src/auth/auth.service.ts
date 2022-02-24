5d97197e2fe43937f258493fbebdf2f2adff1419
VLGR <12@mail.ru>
2022-02-24 23:25:27 +0600

    auth by email w/ jwt token

--- rest-api-trello/src/auth/auth.service.ts
+++ rest-api-trello/src/auth/auth.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { User } from 'src/users/users.model';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcryptjs';
+
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    
+constructor(private userService: UsersService,
+    private jwtService: JwtService) {}
+
+   async login( userDto: CreateUserDto){
+        const user = await this.validateUser(userDto)
+        return this.generateToken(user)
+    }
+
+    async registration(userDto: CreateUserDto) {
+        const candidate = await this.userService.getUserByEmail(userDto.email);
+        if (candidate) {
+            throw new HttpException('email занят', HttpStatus.BAD_REQUEST);
+        }
+        const hashPassword = await bcrypt.hash(userDto.password, 5);
+        const user = await this.userService.createUser({...userDto, password: hashPassword})
+        return this.generateToken(user)
+    }
+    private async generateToken(user:User) {
+        const payload = {email: user.email, id:user.id, roles: user.roles}
+        return {token: this.jwtService.sign(payload)}
+    }
+    private async validateUser(userDto:CreateUserDto) {
+            const user = await this.userService.getUserByEmail(userDto.email)
+            const passwordEquels = await bcrypt.compare(userDto.password, user.password)
+            if (user && passwordEquels) {
+                return user;
+            }
+            throw new UnauthorizedException ('wrong pass')
+    }
+}
